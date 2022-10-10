5252e8dcafd823d7f0b3219ae28f082d78204e4d
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2022-10-10 19:40:38 +0530

    def prime(x):     count=0     for i in range(1,x+1):         if x%i==0:             count += 1     return count n=int(input()) x=m=n while prime(n)!=2:     n += 1 y=n-m while prime(x)!=2:     x -= 1 z=m-x if y>z:     print(z) else:     print(y)

--- Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -2,20 +2,18 @@ def prime(x):
     count=0
     for i in range(1,x+1):
         if x%i==0:
-            count +=1
+            count += 1
     return count
 n=int(input())
-m=p=n
-if prime(n)==2:
-    print(0)
+x=m=n
+while prime(n)!=2:
+    n += 1
+y=n-m
+while prime(x)!=2:
+    x -= 1
+z=m-x
+if y>z:
+    print(z)
 else:
-    while prime(n)!=2:
-        n+=1
-    x=(n-m)
-    while prime(m)!=2:
-        m -= 1
-    y=(p-m)
-    if x<y:
-        print(x)
-    else:
-        print(y)
+    print(y)
+
