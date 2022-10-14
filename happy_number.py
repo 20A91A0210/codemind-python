b9d2f16fddd2912e89d16b309759f9069bf39497
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2022-10-14 11:25:07 +0530

    def hn(x):     sum=0     while x!=0:         r = x%10         sum = sum+ r**2         x //=10     return sum n=int(input()) while n>9:     n=hn(n) if n==1 or n==7:     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,22 +1,14 @@
-def isHappyNumber(num):    
-    rem = sum = 0;    
-        
-    #Calculates the sum of squares of digits    
-    while(num > 0):    
-        rem = num%10;    
-        sum = sum + (rem*rem);    
-        num = num//10;    
-    return sum;    
-        
-num = int(input());    
-result = num;    
-     
-while(result != 1 and result != 4):    
-    result = isHappyNumber(result);    
-     
-#Happy number always ends with 1    
-if(result == 1):    
-    print( "True")    
-#Unhappy number ends in a cycle of repeating numbers which contain 4    
-elif(result == 4):    
-    print("False");
+def hn(x):
+    sum=0
+    while x!=0:
+        r = x%10
+        sum = sum+ r**2
+        x //=10
+    return sum
+n=int(input())
+while n>9:
+    n=hn(n)
+if n==1 or n==7:
+    print(True)
+else:
+    print(False)
