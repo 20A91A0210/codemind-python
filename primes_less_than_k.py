a9e1bd0bd50963770f04074daae6166cf906c3fb
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2022-10-29 20:10:04 +0530

    def prime(n):     prime=True     if n==1:         prime=False     if n==2:         prime =True     for i in range(2,int(n**0.5)+1):         if n%i==0:             prime=False     return prime n=int(input()) lst=list(map(int,input().split())) k=int(input()) c=0 for i in lst:     if prime(i)==True and i<=k:         c+= 1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,18 @@
+def prime(n):
+    prime=True
+    if n==1:
+        prime=False
+    if n==2:
+        prime =True
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            prime=False
+    return prime
+n=int(input())
+lst=list(map(int,input().split()))
+k=int(input())
+c=0
+for i in lst:
+    if prime(i)==True and i<=k:
+        c+= 1
+print(c)
