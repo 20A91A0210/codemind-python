e3aa2d9bac334e7221bbec0f3e2d13d2d728e86f
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2022-11-05 06:03:16 +0530

    n=int(input()) lst=list(map(int,input().split())) sf=ss=0 for i in range(n//2):     x=lst[i]     sf += x for j in range(n//2,n):     y=lst[j]     ss += y print(sf) print(ss)

---
+++ sum_of_first_half_and_second_half_in_an_array.py
@@ -0,0 +1,11 @@
+n=int(input())
+lst=list(map(int,input().split()))
+sf=ss=0
+for i in range(n//2):
+    x=lst[i]
+    sf += x
+for j in range(n//2,n):
+    y=lst[j]
+    ss += y
+print(sf)
+print(ss)
