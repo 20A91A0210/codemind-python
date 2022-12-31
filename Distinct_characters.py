0d3bdb5bb7147897d35c74618ade45c018a89e81
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2022-12-31 18:19:46 +0530

    s=input() s=s.lower() l=[] s1='' for i in s:     if i not in l and i!=' ':         l.append(i) l.sort() for j in l:     s1 += j print(s1)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,12 +1,11 @@
 s=input()
 s=s.lower()
 l=[]
+s1=''
 for i in s:
-    if s.count(i)==1:
+    if i not in l and i!=' ':
         l.append(i)
 l.sort()
-x=''
-for i in l:
-    if i!=' ':
-        x += i
-print(x)
+for j in l:
+    s1 += j
+print(s1)
