ffb7f75b8935ac165a09418a3436735e75af491c
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2022-12-31 16:48:41 +0530

    def vowels(s):     nv=0     for i in s:         if i in 'aeiouAEIOU':             nv+= 1     return nv s=list(map(str,input().split())) l=[] count=0 for i in s:     l.append(vowels(i)) print(*l)

---
+++ vowel_count_in_each_word.py
@@ -0,0 +1,12 @@
+def vowels(s):
+    nv=0
+    for i in s:
+        if i in 'aeiouAEIOU':
+            nv+= 1
+    return nv
+s=list(map(str,input().split()))
+l=[]
+count=0
+for i in s:
+    l.append(vowels(i))
+print(*l)
