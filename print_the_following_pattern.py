1068b928633dd62c7e06c47e873a20661c25b59e
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2023-01-05 19:24:52 +0530

    n=int(input()) for row in range(n):     for col in range(n,0,-1):         print(col,end=' ')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,7 +1,6 @@
 n=int(input())
-x=65+n-1
-for row in range(n,0,-1):
-    for col in range(row):
-        print(chr(x),end=' ')
-    x -= 1
+for row in range(n):
+    for col in range(n,0,-1):
+        print(col,end=' ')
     print()
+        
