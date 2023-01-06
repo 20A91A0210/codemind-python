de2618231ac16fdb4d8e8ca0ae9577d974a617a5
vamsi krishna <109405897+20A91A0210@users.noreply.github.com>
2023-01-06 20:20:25 +0530

    n=int(input()) i=1 for row in range(n,0,-1): #5,4,3,2,1  #4,3,2,1     for col in range(i,n+1):  #1,2,3,4,5     # 2,3,4,5         print(col,end=' ')     i +=1     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,10 +1,7 @@
 n=int(input())
-x=n
-for row in range(n):
-    for col in range(n):
-        if row==col or row+col==n-1:
-            print(x,end=' ')
-        else:
-            print(end=' ')
-    x -= 1
+i=1
+for row in range(n,0,-1): #5,4,3,2,1  #4,3,2,1
+    for col in range(i,n+1):  #1,2,3,4,5     # 2,3,4,5  
+        print(col,end=' ')
+    i +=1
     print()
